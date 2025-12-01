f58761ef06afa390bc7a78b020c47f5085e45d01
NAYAN ACHARYA <100992818+nayan135@users.noreply.github.com>
2025-12-01 08:57:38 +0545

    pushhhhhhingggg

--- lib/home.dart
+++ lib/home.dart
@@ -12,21 +12,51 @@ class HomePage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Container(
+           child:ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(15),
+              ),
+            child: Container(
+              height: 350,
+              width: 300,
+              color: Colors.blueAccent.withOpacity(0.2),
+               child: Text(
+            'Hellooo Welcome to the Den',
+            style: TextStyle(fontSize: 48),
+           ),
+              
+            ),
 
-           CircleAvatar(
+            //backgroundd baabl banaunu parxȧ hai ta aajalai yeti nai 
+           
+          
+          ),
+          ),
+         const SizedBox(height:10),
+Padding(
+            padding: EdgeInsets.all(10),
+         
+         
+          
+        ),
+         
+
+        ],
+          ),
+          /* CircleAvatar(
             
           radius: 28,
           backgroundImage: NetworkImage('https://n4ce5f0tqvsfhe7l.public.blob.vercel-storage.com/nayan-acharya.jpg'),
             
         
            ),
+           */
            
-          const SizedBox(height:10),
-
-    ]
-      ),
-      );
-      
+         
+    
+    
+    );
 
       }
     
